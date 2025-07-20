d37cb78c8b5db0c060311d34c4824d0971812407
gopalnadell <gopalnadell@gmail.com>
2025-07-20 09:22:42 +0530

    dummy check in

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "example1" {
+resource "aws_s3_bucket" "example" {
   bucket        = "gk-state-file-cicd"
   force_destroy = true
 }
